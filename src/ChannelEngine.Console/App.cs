27b335b9859d270d28172ee7305a20d4c1f87692
Saeed Salehi <s.salehi@mootanroo.com>
2022-04-14 20:15:04 +0430

    add console project

---
+++ src/ChannelEngine.Console/App.cs
@@ -0,0 +1,83 @@
+﻿using ChannelEngine.Core;
+using ChannelEngine.Core.DTOs;
+using ChannelEngine.Services.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+public class App
+{
+    private readonly ILogger<App> _logger;
+    private readonly OrderService _orderService;
+    private readonly Settings _appSettings;
+
+    public App(IOptions<Settings> appSettings,
+        ILogger<App> logger,
+        OrderService orderService)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _orderService = orderService;
+        _appSettings = appSettings?.Value ?? throw new ArgumentNullException(nameof(appSettings));
+    }
+
+    public async Task Run(string[] args)
+    {
+        _logger.LogInformation("Starting...");
+
+        Console.WriteLine();
+        Console.WriteLine($"Api Key {_appSettings.ChannelEngine.ApiKey}");
+
+        await GetAllOrders();
+
+        await Top5Products();
+        
+        await UpdateStock();
+
+        _logger.LogInformation("Finished!");
+        await Task.CompletedTask;
+    }
+
+
+    private async Task UpdateStock()
+    {
+        Console.Write($"{Environment.NewLine} Please enter the product no: ");
+        var productNo = Console.ReadLine();
+
+        //TODO: validate productNo
+
+        Console.Write($"{Environment.NewLine} Please enter the new stock quantity: ");
+        var quantity = int.Parse(Console.ReadLine());
+
+        // TODO: validate quantity
+
+        var result = await _orderService.UpdateStock(productNo).ConfigureAwait(false);
+
+        Console.Write($"{Environment.NewLine} Product stock updated ({result})");
+    }
+
+
+    private async Task Top5Products()
+    {
+        Console.WriteLine("Getting Top 5 Sold products!");
+
+        var result = await _orderService.GetTopSoldProducts().ConfigureAwait(false);
+
+        var serialzied = JsonConvert.SerializeObject(result);
+        Console.WriteLine(serialzied);
+    }
+
+    private async Task<IEnumerable<Orders>> GetAllOrders()
+    {
+        Console.WriteLine("Getting all IN_PROGRESS products");
+        var result = await _orderService.GetAllInProgressOrdersAsync().ConfigureAwait(false);
+
+        Console.WriteLine($"Orders count : {result.Count()}");
+
+        var serialzied = JsonConvert.SerializeObject(result);
+        Console.WriteLine(serialzied);
+
+        return result;
+    }
+}
---
+++ src/ChannelEngine.Console/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ChannelEngine.Console;
+
+using System.IO;
+using System.Threading.Tasks;
+using ChannelEngine.Services.HttpClients;
+using ChannelEngine.Services.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // create service collection
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+
+        // create service provider
+        var serviceProvider = services.BuildServiceProvider();
+
+        // entry to run app
+        await serviceProvider.GetService<App>().Run(args);
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        // configure logging
+        services.AddLogging(builder =>
+        {
+            builder.AddConsole();
+            builder.AddDebug();
+        });
+
+        // build config
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddEnvironmentVariables()
+            .Build();
+
+
+        // add services:
+
+        // add app
+        services.AddTransient<App>();
+        //Register Services in DI
+        services.AddTransient<OrderService>();
+
+        //services.AddHttpClient<OrderHttpClient>(client =>
+        //{
+        //    client.BaseAddress = new Uri(Configuration["ChannelEngine:BaseUrl"]);
+        //});
+    }
+}
+

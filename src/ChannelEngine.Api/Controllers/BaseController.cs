f476eadf4a7aafd0e24f61feccf4be882b09aa5e
Saeed Salehi <s.salehi@mootanroo.com>
2022-04-14 20:57:34 +0430

    add base controller for mvc

---
+++ src/ChannelEngine.Api/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ChannelEngine.Api.Controllers;
+
+public abstract class BaseController : Controller
+{
+    private readonly ILogger logger;
+
+    public BaseController(ILogger logger) => this.logger = logger;
+
+    public override void OnActionExecuted(ActionExecutedContext context)
+    {
+        if (context.Exception != null)
+        {
+            logger.LogError(context.Exception, "exception occured during process request!");
+        }
+
+        base.OnActionExecuted(context);
+    }
+}

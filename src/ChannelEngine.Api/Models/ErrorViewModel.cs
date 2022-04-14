4af129f2bfdc15ee53d21c2b7d48c24c917272a5
Saeed Salehi <s.salehi@mootanroo.com>
2022-04-14 20:58:34 +0430

    add  mvc related models

---
+++ src/ChannelEngine.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ChannelEngine.Api.Models;
+
+public class ErrorViewModel
+{
+
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+}
---
+++ src/ChannelEngine.Api/Models/OrdersViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ChannelEngine.Core.DTOs;
+
+namespace ChannelEngine.Api.Models;
+
+public class OrdersViewModel
+{
+    public List<OrderDto> Orders { get; set; }
+    public List<ProductDto> TopSoldProducts { get; set; }
+}
